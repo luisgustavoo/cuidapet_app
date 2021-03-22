55db74c076a70bbe31428a9c97ebd3bceaa20767
Luis Gustavo <luisgustavovieirasantos@gmail.com>
2021-03-21 23:04:30 -0300

    Ajuste de imports no arquivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-
 import 'app/app_module.dart';
 
 void main() {
